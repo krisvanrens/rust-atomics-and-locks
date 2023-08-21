46b54181ce7c1ef831c4b432dbeb2c76416ddbb3
Kris van Rens <krisvanrens@gmail.com>
2023-08-21 23:56:24 +0200

    ADD: Checked version of unsafe channel.

---
+++ ch05-channel/src/channel_oneshot_checked.rs
@@ -0,0 +1,75 @@
+use std::{
+    cell::UnsafeCell,
+    mem::MaybeUninit,
+    sync::atomic::{AtomicBool, Ordering},
+};
+
+#[cfg(test)]
+use std::{thread, time::Duration};
+
+pub struct Channel<T> {
+    value: UnsafeCell<MaybeUninit<T>>,
+    occupied: AtomicBool,
+    ready: AtomicBool,
+}
+
+// Tell the compiler our type is Sync as long as T is Send (required because UnsafeCell is not Send only).
+unsafe impl<T> Sync for Channel<T> where T: Send {}
+
+impl<T> Channel<T> {
+    pub const fn new() -> Self {
+        Self {
+            value: UnsafeCell::new(MaybeUninit::uninit()),
+            occupied: AtomicBool::new(false),
+            ready: AtomicBool::new(false),
+        }
+    }
+
+    pub fn send(&self, value: T) {
+        if self.occupied.swap(true, Ordering::Relaxed) {
+            panic!("calling send on an occupied channel");
+        }
+
+        unsafe { (*self.value.get()).write(value) };
+        self.ready.store(true, Ordering::Release);
+    }
+
+    pub fn receive(&self) -> T {
+        if !self.ready.swap(false, Ordering::Acquire) {
+            panic!("calling receive on an empty channel");
+        }
+
+        unsafe { (*self.value.get()).assume_init_read() }
+    }
+
+    pub fn is_ready(&self) -> bool {
+        self.ready.load(Ordering::Acquire)
+    }
+}
+
+impl<T> Drop for Channel<T> {
+    fn drop(&mut self) {
+        if *self.ready.get_mut() {
+            unsafe { self.value.get_mut().assume_init_drop() };
+        }
+    }
+}
+
+#[test]
+fn test_channel() {
+    let c = Channel::<i32>::new();
+
+    thread::scope(|s| {
+        s.spawn(|| {
+            c.send(42);
+        });
+
+        s.spawn(|| {
+            while !c.is_ready() {
+                thread::sleep(Duration::from_millis(10));
+            }
+
+            assert_eq!(c.receive(), 42);
+        });
+    });
+}
--- ch05-channel/src/lib.rs
+++ ch05-channel/src/lib.rs
@@ -1,3 +1,4 @@
+pub mod channel_oneshot_checked;
 pub mod channel_oneshot_option;
 pub mod channel_oneshot_unsafe;
 pub mod channel_simple;
