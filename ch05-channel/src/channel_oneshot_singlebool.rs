3f3fa5b4b7178d1d74db3ee217d7e8e82da79fa1
Kris van Rens <krisvanrens@gmail.com>
2023-08-24 22:47:03 +0200

    ADD: Single-bool version of one-shot channel.

---
+++ ch05-channel/src/channel_oneshot_singlebool.rs
@@ -0,0 +1,108 @@
+use std::{
+    cell::UnsafeCell,
+    mem::MaybeUninit,
+    sync::atomic::{AtomicU8, Ordering},
+};
+
+#[cfg(test)]
+use std::{thread, time::Duration};
+
+const EMPTY: u8 = 0;
+const WRITING: u8 = 1;
+const READY: u8 = 2;
+const READING: u8 = 3;
+
+pub struct Channel<T> {
+    value: UnsafeCell<MaybeUninit<T>>,
+    state: AtomicU8,
+}
+
+// Tell the compiler our type is Sync as long as T is Send (required because UnsafeCell is not Send only).
+unsafe impl<T> Sync for Channel<T> where T: Send {}
+
+impl<T> Channel<T> {
+    pub const fn new() -> Self {
+        Self {
+            value: UnsafeCell::new(MaybeUninit::uninit()),
+            state: AtomicU8::new(EMPTY),
+        }
+    }
+
+    pub fn send(&self, value: T) {
+        //
+        // State: EMPTY --> WRITING
+        //
+        // ...
+        //
+        if self
+            .state
+            .compare_exchange(EMPTY, WRITING, Ordering::Relaxed, Ordering::Relaxed)
+            .is_err()
+        {
+            panic!("calling send on an occupied channel");
+        }
+
+        unsafe { (*self.value.get()).write(value) };
+
+        //
+        // State: WRITING --> READY
+        //
+        // ...
+        //
+        self.state.store(READY, Ordering::Release);
+    }
+
+    pub fn receive(&self) -> T {
+        //
+        // State: READY --> READING
+        //
+        // ...
+        //
+        if self
+            .state
+            .compare_exchange(READY, READING, Ordering::Acquire, Ordering::Relaxed)
+            .is_err()
+        {
+            panic!("calling receive on an empty channel");
+        }
+
+        unsafe { (*self.value.get()).assume_init_read() }
+
+        //
+        // State: READING --> EMPTY
+        //
+        // ...
+        //
+    }
+
+    pub fn is_ready(&self) -> bool {
+        self.state.load(Ordering::Relaxed) == READY
+    }
+}
+
+impl<T> Drop for Channel<T> {
+    fn drop(&mut self) {
+        if *self.state.get_mut() == READY {
+            unsafe { self.value.get_mut().assume_init_drop() };
+        }
+    }
+}
+
+#[test]
+fn test_channel() {
+    let c = Channel::<i32>::new();
+
+    thread::scope(|s| {
+        s.spawn(|| {
+            c.send(42);
+        });
+
+        s.spawn(|| {
+            while !c.is_ready() {
+                thread::sleep(Duration::from_millis(10));
+            }
+
+            assert_eq!(c.receive(), 42);
+        });
+    });
+}
--- ch05-channel/src/lib.rs
+++ ch05-channel/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod channel_oneshot_checked;
 pub mod channel_oneshot_option;
+pub mod channel_oneshot_singlebool;
 pub mod channel_oneshot_unsafe;
 pub mod channel_simple;
